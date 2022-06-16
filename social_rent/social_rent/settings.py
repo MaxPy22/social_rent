5738de3b0893309bbcb28974f8d7158aee7629d9
MaxPy22 <onlyinfo@ya.ru>
2022-06-16 19:15:41 +0300

    projekto karkasas

--- social_rent/social_rent/settings.py
+++ social_rent/social_rent/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9+1_$bt&y^f_0wczax0l)lajy6y!j41wd0)!9v2_8g=rhdxy2c'
+SECRET_KEY = 'django-insecure-jr1g-+u%l#ph@tav_5$nl1j*e51uz@+hf6u3ey5-es2kt!-(g-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
